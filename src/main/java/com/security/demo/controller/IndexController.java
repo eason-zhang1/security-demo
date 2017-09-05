8e891c8fb9cf92e18846c775f75e63d0b829816e
张海军 <739663407@qq.com>
2017-09-05 17:58:59 +0800

    国际化配置

--- src/main/java/com/security/demo/controller/IndexController.java
+++ src/main/java/com/security/demo/controller/IndexController.java
@@ -5,9 +5,13 @@ import com.security.demo.jpa.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 @RequestMapping("/api/public")
@@ -16,9 +20,18 @@ public class IndexController {
   @Autowired
   private UserRepository userRepository;
 
+  @Autowired
+  private MessageSource messageSource;
+
   @GetMapping("/index")
   @Cacheable(value = "test:cache:methods", key = "#root.method.name")
   public User index(){
     return userRepository.findOne(2L);
+  }
+
+  @GetMapping("/index2")
+  public String index2(){
+    Locale locale = LocaleContextHolder.getLocale();//zh_CN
+    return messageSource.getMessage("welcome", null, null);
   }
 }
